0d752b102d8b8cab9fb65dfa274fe6a9d6c93446
gaurav <icon.gaurav806@gmail.com>
2025-08-21 00:25:12 +0530

    error message added

--- components/WalletConnect.tsx
+++ components/WalletConnect.tsx
@@ -5,7 +5,7 @@ import {useAccount, useConnect, useDisconnect} from "wagmi";
 export default function WalletConnect({ onAddWallet }: {
     onAddWallet: (address: string) => void
 }) {
-    const {connectAsync, connectors, status , reset} = useConnect();
+    const {connectAsync, connectors, status , reset, error} = useConnect();
     const { disconnect } = useDisconnect();
     const handleConnect = async () => {
         try {
@@ -20,39 +20,10 @@ export default function WalletConnect({ onAddWallet }: {
         }
     }
 
-    console.log(status)
 
     return (
         <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-2xl shadow-md">
             <h2 className="text-xl font-semibold text-gray-800 mb-4">Add Wallet</h2>
-
-            {/* Option 1: Add manually */}
-            {/*<div className="mb-6">*/}
-            {/*    <label className="block text-sm font-medium text-gray-700 mb-2">*/}
-            {/*        Enter Wallet Address*/}
-            {/*    </label>*/}
-            {/*    <div className="flex gap-2">*/}
-            {/*        <input*/}
-            {/*            type="text"*/}
-            {/*            placeholder="0x123..."*/}
-            {/*            value={address}*/}
-            {/*            onChange={(e) => setAddress(e.target.value)}*/}
-            {/*            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"*/}
-            {/*        />*/}
-            {/*        <button*/}
-            {/*            onClick={() => onAddWallet(address)}*/}
-            {/*            className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition"*/}
-            {/*        >*/}
-            {/*            Save*/}
-            {/*        </button>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-
-            {/*<div className="flex items-center gap-2 mb-6">*/}
-            {/*    <div className="flex-grow border-t border-gray-300"></div>*/}
-            {/*    <span className="text-sm text-gray-500">OR</span>*/}
-            {/*    <div className="flex-grow border-t border-gray-300"></div>*/}
-            {/*</div>*/}
 
             {/* Option 2: Connect wallet */}
             <div className="text-center">
@@ -62,6 +33,8 @@ export default function WalletConnect({ onAddWallet }: {
                 >
                     {status === 'pending'? 'Connecting...' :'Connect Using External Wallet'}
                 </button>
+
+                {error && <p className="text-red-600 mt-2">{error.message}</p>}
             </div>
         </div>
     )
